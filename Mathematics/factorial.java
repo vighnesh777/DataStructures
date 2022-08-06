8bb937b1d146a2fe7205c72208f4f9f904d93a22
Vighnesh Kanakala <vkanakala@zscaler.com>
2022-08-06 14:37:50 +0530

    New File

---
+++ Mathematics/factorial.java
@@ -0,0 +1,28 @@
+package Mathematics;
+import java.util.*;
+public class factorial {
+    public int factorial(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+    public int factorialIterative(int n) {
+       if(n==0)return 1;
+       n=Math.abs(n);
+       int fact=1;
+       for(int i=n;i>=1;i--)
+       {
+           fact*=i;
+       }
+       return fact;
+    }
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            System.out.println("Iterative :  "+new factorial().factorialIterative(n));
+            System.out.println("Recursive :  "+new factorial().factorial(n));
+            
+        }
+    }
+}
---
+++ Mathematics/trailingZeroes.java
@@ -0,0 +1,35 @@
+package Mathematics;
+import java.util.*;
+public class trailingZeroes {
+    public int countZeroes(int n)
+    {
+        int fact=new factorial().factorial(n);
+        int count=0;
+        
+        while(fact%10==0)
+        {
+            count++;
+            fact/=10;
+        }
+        return count;
+    }
+    public int countZeroesIterative(int n)
+    {
+        int count=0;
+       for(int i=5;i<=n;i*=5)
+       {
+              count+=n/i;   
+
+       }
+         return count;
+    }
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            System.out.println("Iterative :  "+new trailingZeroes().countZeroesIterative(n));
+            if(n<10)
+            System.out.println("Iterative 1 :  "+new trailingZeroes().countZeroes(n));
+            
+        }
+    }
+}
